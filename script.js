e3893bce94e6f4e57a636b21f1ceb26952a3dd84
manichandanreddy14 <suddalamanichandanreddy62@gmail.com>
2023-12-16 21:16:53 +0530

    final

---
+++ script.js
@@ -0,0 +1,14 @@
+//https://github.com/manichandanreddy14/f3-contest1-dec.git
+// {/* <style src="./sytel.css">
+// .head{
+//     background-color: black;
+//     height:  20vh;
+//     margin: 0;
+// }
+// .text{
+//     color: white;
+//    text-align: center;
+//    padding-top:40px ;
+//    scale: 1.5;
+// }
+// <div class="head"><header class="text"><span style="color: > */}
